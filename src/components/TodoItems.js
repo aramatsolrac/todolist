0de162a859890e3ff3b8c3dff50d257b1eb7328e
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-02-21 18:21:28 -0700

    Add delete function

---
+++ src/components/TodoItems.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./TodoItems.scss";
+
+class TodoItems extends Component {
+  constructor(props) {
+    super(props);
+
+    this.createTasks = this.createTasks.bind(this);
+  }
+
+  delete(key) {
+    this.props.delete(key);
+  }
+
+  createTasks(item) {
+    return (
+      <div className="todo__list">
+        <div className="todo__items">
+          <div className="todo__task">
+            <input type="checkbox" key={item.key} />
+            <label>{item.text}</label>
+          </div>
+          <div>
+            <button
+              className="fas fa-trash"
+              onClick={() => this.delete(item.key)}
+            ></button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTasks);
+
+    return <ul className="todo">{listItems}</ul>;
+  }
+}
+
+export default TodoItems;
---
+++ src/components/TodoList.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.scss";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  addItem(item) {
+    if (this._inputElement.value !== "") {
+      let newItem = {
+        text: this._inputElement.value,
+        key: Date.now(),
+      };
+
+      this.setState((prevState) => {
+        return {
+          items: prevState.items.concat(newItem),
+        };
+      });
+
+      this._inputElement.value = "";
+    }
+
+    console.log(this.state.items);
+
+    item.preventDefault();
+  }
+
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function (item) {
+      return item.key !== key;
+    });
+
+    this.setState({
+      items: filteredItems,
+    });
+  }
+  render() {
+    return (
+      <div className="todo__list">
+        <form className="todo__form" onSubmit={this.addItem}>
+          <input
+            ref={(a) => (this._inputElement = a)}
+            placeholder="Enter task"
+            className="todo__input"
+          ></input>
+          <button type="submit" className="todo__button">
+            Add
+          </button>
+        </form>
+        <TodoItems entries={this.state.items} delete={this.deleteItem} />
+      </div>
+    );
+  }
+}
+
+export default TodoList;
