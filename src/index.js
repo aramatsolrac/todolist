6dac382e38e02f6763a558c14e9d3564b3b4c8ae
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-02-21 18:21:09 -0700

    Improve style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
-import TodoList from "./TodoList";
 
 const root = document.getElementById("root");
 
